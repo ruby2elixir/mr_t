ed5649507cce9f01a1f24645e74ac6a5fff03c3b
Roman Heinrich <roman.heinrich@gmail.com>
2016-08-09 20:08:01 +0200

    fix: still need the extra require in Monitor.Src
    problem: 
    - on syntax errors the src monitor is not restarted, if we just recompile
    
    
    solution: 
    - revert back to extra Utils.require_file call
    - ignore events argument

--- lib/mr_t/monitor/src.ex
+++ lib/mr_t/monitor/src.ex
@@ -5,8 +5,9 @@ defmodule MrT.Monitor.Src do
     IO.puts "STOP"
   end
 
-  def callback(file_path, events) do
+  def callback(file_path, _events) do
     if (Path.extname file_path) in MrT.Config.src_extensions do
+      MrT.Utils.require_file(file_path)
       MrT.Utils.recompile
       MrT.EventBus.event_for_tests(file_path)
     end
