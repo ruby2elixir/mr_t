9c9e6567e0a7aa2e0591f9f6f595d77f9a516004
Roman Heinrich <roman.heinrich@gmail.com>
2016-08-09 19:02:40 +0200

    removes code for module deletion in ExUnit runner
    - while maybe not as clean for long running applications, it does not matter, if some modules linger in memory (they are not executed later)

--- lib/mr_t/runner/ex_unit.ex
+++ lib/mr_t/runner/ex_unit.ex
@@ -26,19 +26,14 @@ defmodule MrT.Runner.ExUnit do
   end
 
   def require_run_cleanup(task, test_files) do
-    # IO.puts "running tests..."
-    modules = require_test_helper
-    modules = modules ++ load_modules(test_files)
+    require_test_helper
+    load_modules(test_files)
     ExUnit.Server.cases_loaded()
     %{failures: _failures} = results = Task.await(task, :infinity)
-    # FIXME: this might be not necessary due to direct code loading
-    modules |> delete_modules
     {:ok, results}
   end
 
   def load_modules(test_files) do
-    ### Kernel.ParallelRequire.files is problematic due to race conditions on syntax errors...
-    # modules = test_files |> Kernel.ParallelRequire.files([each_module: fn(x,y,z)-> IO.inspect({x, y ,z}) end ])
     test_files |> Enum.map(&MrT.Utils.require_file/1) |> List.flatten
   end
 
@@ -51,15 +46,6 @@ defmodule MrT.Runner.ExUnit do
     files
     |> Enum.map(&Path.expand/1)
     |> Code.unload_files
-  end
-
-  def delete_modules(modules) do
-    modules |> Enum.map(&delete_module/1)
-  end
-
-  def delete_module(m) do
-    m |> :code.purge
-    m |> :code.delete
   end
 
   def doit(files) do
