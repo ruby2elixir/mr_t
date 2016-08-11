7b7cb0e664cce0d2da890c1c839eb0658e58a3b6
Roman Heinrich <roman.heinrich@gmail.com>
2016-08-11 11:38:17 +0200

    check for file existance before requiring it
    problem: 
    - some editors create tmp files, that match the watched file extensions
    - that causes problems when requiring them
    
    solution: 
    - check for file existence before requiring it

--- lib/mr_t/utils.ex
+++ lib/mr_t/utils.ex
@@ -26,6 +26,11 @@ defmodule MrT.Utils do
   compiles the module in memory on loading, for quicker turnaround
   """
   def require_file(file) do
+    require_file(File.exists?(file), file)
+  end
+  def require_file(false, file), do: puts "not found file #{file}"
+  def require_file(true,  file) do
+    puts "require #{file}"
     Code.compiler_options(ignore_module_conflict: true) # prevents "warning: redefining module Foo"
     res = :elixir_compiler.file(file) |> Enum.map(fn({m,_b})-> m end)
     Code.compiler_options(ignore_module_conflict: false)
