148d81a4aab433bd441bcf89e20ba668a69918d7
Roman Heinrich <roman.heinrich@gmail.com>
2016-08-12 10:43:53 +0200

    minor edits

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -6,11 +6,11 @@ defmodule MrT.Utils.Test do
 
   describe "require_file" do
     test "does not blow up on tmp files" do
-      MrT.Utils.verbosity_on
+      Utils.verbosity_on
       assert capture_io(fn->
-        MrT.Utils.require_file("i_dont_exist.ex")
+        Utils.require_file("i_dont_exist.ex")
       end) == "not found file i_dont_exist.ex\n"
-      MrT.Utils.verbosity_off
+      Utils.verbosity_off
     end
   end
 end
